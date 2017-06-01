6cda84420ca06ce1f614ff5e308028233ec30e77
Reese De Wind <Reese.DeWind@gmail.com>
2017-05-31 20:32:36 -0400

    completed some of the more interesting excercises in chapter 4

---
+++ Chapter4/Chapter4.hs
@@ -0,0 +1,31 @@
+-- Write a function splitWith that acts similarly to words, but takes a predicate and a list of any type, and splits its input list on every element for which the predicate returns False
+splitWith :: (a -> Bool) -> [a] -> [[a]]
+
+splitWith _ [] = [[]]
+splitWith _ (x:xs) | null xs = [[x]]
+splitWith p xs =
+  let (pre, suf) = break p (dropWhile p xs)
+  in pre : (splitWith p suf)
+
+
+-- Write your own definition of concat using foldr.
+myConcat :: [[a]] -> [a]
+myConcat xs = foldr (\x y -> foldr (:) y x) [] xs
+--myConcat = foldr (++) []
+
+
+-- Write your own definition of the standard takeWhile function, first using explicit recursion, then foldr
+myTakeWhile  :: (a -> Bool) -> [a] -> [a]
+myTakeWhile _ [] = []
+myTakeWhile _ (x:xs) | null xs = [x]
+myTakeWhile p (x:xs) | not (p x) = []
+myTakeWhile p (x:xs) = x : myTakeWhile p xs
+
+
+foldrTakeWhile :: (a -> Bool) -> [a] -> [a]
+foldrTakeWhile _ [] = []
+foldrTakeWhile _ (x:xs) | null xs = [x]
+foldrTakeWhile p (x:xs) | not (p x) = []
+foldrTakeWhile p xs = foldr func [] xs
+  where func x y | p x = x : y
+                 | otherwise = []
